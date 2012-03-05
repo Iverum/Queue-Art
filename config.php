74109139001b716d05577e7627ba938473373107
Blake A. Hair <contact@redono.org>
2012-03-05 02:36:27 -0500

    Tagging 1.0

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 	// The API key provided by Flickr for your application.
-	define('API_KEY', /*API KEY GOES HERE*/'');
+	define('API_KEY', /*API KEY GOES HERE*/' ');
 	// The ID of the gallery that photos will be pulled from.
 	define('GAL_ID', '32523522-72157629132207964');
 	// Should the application hold persistent links to images?
